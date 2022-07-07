523cf97cba2228e0395f0aa1eea40d19135bf796
Obakeng <mosadiobakeng7@gmail.com>
2022-07-07 12:03:59 +0200

    Added Twilio credentials

--- server/venv/main.py
+++ server/venv/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
+import os
+import twilio.jwt.access_token
+import twilio.jwt.access_tokne.grants
+import twilio.rest
 
+# Twilio Credentials
+account_sid = os.environ['TWILIO_ACC_SID']
+api_secret = os.environ['TWILIO_API_SECRET']
+api_key = os.environ['TWILIO_API_KEY']
+
+# Create a FastAPI app
 app = FastAPI()
 
 
