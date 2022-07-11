1d8e94221e1fc6d13cc2dd0b295a8a7177a119fb
Obakeng <mosadiobakeng7@gmail.com>
2022-07-11 09:26:19 +0200

    Created new div container

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,9 +2,14 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
+    <>
     <div className='text-5xl underline font-bold text-blue-600'>
       Toogle Meet
     </div>
+    <div>
+      
+    </div>
+    </>
   )
 }
 
