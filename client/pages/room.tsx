3a41d21a0d320134d6a77f5a446ecba47b8bf47b
Obakeng <mosadiobakeng7@gmail.com>
2022-07-11 09:58:58 +0200

    rounded corners for mute button

--- client/pages/room.tsx
+++ client/pages/room.tsx
@@ -6,8 +6,8 @@ function Room() {
             <div className="video border border-blue-500 h-72 w-72">
                 Participant 1
             </div>
-            <div className="tracks flex space-x-6 justify-center items-center">
-                <button className="bg-gray-900 font-bold p-2 text-white">Mute</button>
+            <div className="tracks my-4 flex space-x-6 justify-center items-center">
+                <button className="bg-gray-900 font-bold p-2 rounded-md text-white">Mute</button>
                 <button className="bg-red-600 font-bold p-2 text-white">End call</button>
             </div>
         </div>
