ef5a5b067f8225ba03019209109444cb99a6aae9
Obakeng <mosadiobakeng7@gmail.com>
2022-07-11 09:36:05 +0200

    Space between join room elements

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
       <div className='text-5xl underline font-bold text-blue-600'>
         Toogle Meet
       </div>
-      <div className='flex flex-col my-6'>
+      <div className='flex flex-col my-6 space-y-3'>
         <input type={"text"} className='p-3'/>
         <button className='bg-gradient-to-r from-cyan-500 to-blue-500 p-3 text-white font-bold'>Join</button>
       </div>
