305c6ec904ffff0d3b57fb05c6ca3a2d0e1b8995
Obakeng <mosadiobakeng7@gmail.com>
2022-07-11 09:29:48 +0200

    Outer div wrapper for all div elements

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,12 +3,14 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <>
-    <div className='text-5xl underline font-bold text-blue-600'>
-      Toogle Meet
-    </div>
-    <div className='flex flex-col'>
-      <input type={"text"}/>
-      <button className='bg-gradient-to-r from-cyan-500 to-blue-500'>Join</button>
+    <div>
+      <div className='text-5xl underline font-bold text-blue-600'>
+        Toogle Meet
+      </div>
+      <div className='flex flex-col'>
+        <input type={"text"}/>
+        <button className='bg-gradient-to-r from-cyan-500 to-blue-500'>Join</button>
+      </div>
     </div>
     </>
   )
