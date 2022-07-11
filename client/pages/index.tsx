06d0414cf90a7ab37fdc86af77ce8f84c95b64cd
Obakeng <mosadiobakeng7@gmail.com>
2022-07-11 09:25:15 +0200

    Added text styling to headline

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>Ayo</div>
+    <div className='text-5xl underline font-bold text-blue-600'>
+      Toogle Meet
+    </div>
   )
 }
 
