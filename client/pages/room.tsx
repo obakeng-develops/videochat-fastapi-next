f045f3c69673490507bd65a94b5bf863096890aa
Obakeng <mosadiobakeng7@gmail.com>
2022-07-11 09:57:55 +0200

    red background and bold font for end button

--- client/pages/room.tsx
+++ client/pages/room.tsx
@@ -8,7 +8,7 @@ function Room() {
             </div>
             <div className="tracks flex space-x-6 justify-center items-center">
                 <button className="bg-gray-900 font-bold p-2 text-white">Mute</button>
-                <button>End call</button>
+                <button className="bg-red-600 font-bold p-2 text-white">End call</button>
             </div>
         </div>
         <div>
