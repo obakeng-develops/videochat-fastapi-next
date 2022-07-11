c8ab0e689dcab3024b55fa8961ad14405af57a83
Obakeng <mosadiobakeng7@gmail.com>
2022-07-11 09:39:34 +0200

    Added margin to join room elements

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -7,7 +7,7 @@ const Home: NextPage = () => {
       <div className='text-6xl underline font-bold text-blue-600'>
         Toogle Meet
       </div>
-      <div className='flex flex-col my-6 space-y-3'>
+      <div className='flex flex-col my-8 space-y-3'>
         <input type={"text"} className='p-3 bg-slate-100' placeholder='Enter room code'/>
         <button className='bg-gradient-to-r from-cyan-500 to-blue-500 p-3 text-white font-bold'>Join</button>
       </div>
