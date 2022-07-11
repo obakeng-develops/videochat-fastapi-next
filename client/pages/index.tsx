59fa31590229f5e2c9b236d3321d2d5474842ffe
Obakeng <mosadiobakeng7@gmail.com>
2022-07-11 09:29:08 +0200

    Added flex column

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
     <div className='text-5xl underline font-bold text-blue-600'>
       Toogle Meet
     </div>
-    <div>
+    <div className='flex flex-col'>
       <input type={"text"}/>
       <button className='bg-gradient-to-r from-cyan-500 to-blue-500'>Join</button>
     </div>
