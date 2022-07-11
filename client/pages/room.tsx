db0c883fefe408cfa446caef00e03569bea68bc0
Obakeng <mosadiobakeng7@gmail.com>
2022-07-11 10:00:03 +0200

    cloned participant 1 div to participant 2

--- client/pages/room.tsx
+++ client/pages/room.tsx
@@ -12,7 +12,13 @@ function Room() {
             </div>
         </div>
         <div>
-            Participant 2
+            <div className="video border border-blue-500 h-72 w-72">
+                Participant 2
+            </div>
+            <div className="tracks my-4 flex space-x-6 justify-center items-center">
+                <button className="bg-gray-900 font-bold p-2 rounded-md text-white">Mute</button>
+                <button className="bg-red-600 font-bold p-2 rounded-md text-white">End call</button>
+            </div>
         </div>
     </div>
     </>
