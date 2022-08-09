3bc33f779b022f035aafc46a731299ca49e7df91
Obakeng <mosadiobakeng7@gmail.com>
2022-08-09 09:43:25 +0200

    feat: add onClick property

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
       </div>
       <div className='flex flex-col my-8 space-y-3'>
         <input type={"text"} className='p-3 bg-slate-100' id="room-name" placeholder='Enter room code'/>
-        <button className='bg-gradient-to-r from-cyan-500 to-blue-500 p-3 text-white font-bold'>Join</button>
+        <button className='bg-gradient-to-r from-cyan-500 to-blue-500 p-3 text-white font-bold' onClick={() => alert(roomName)}>Join</button>
       </div>
     </div>
     </>
