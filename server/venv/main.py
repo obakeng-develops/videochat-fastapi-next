2db9bb378b906cda80963da0f07e5bc733b842cb
Obakeng <mosadiobakeng7@gmail.com>
2022-08-09 11:35:00 +0200

    feat: add cors middleware

--- server/venv/main.py
+++ server/venv/main.py
@@ -22,6 +22,14 @@ origins = [
     "http://127.0.0.1:3000"
 ]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 
 def create_room(room_name: str):
     """
