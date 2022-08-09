9924efa861be945fad3eef03cff32056d7297711
Obakeng <mosadiobakeng7@gmail.com>
2022-08-09 09:56:17 +0200

    feat: add onSubmit handler

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
       <div className='text-6xl underline font-bold text-blue-600'>
         Toogle Meet
       </div>
-      <form className='flex flex-col my-8 space-y-3'>
+      <form className='flex flex-col my-8 space-y-3' onSubmit={handleSubmit}>
         <input type={"text"} className='p-3 bg-slate-100' id="room-name" placeholder='Enter room code'/>
         <button className='bg-gradient-to-r from-cyan-500 to-blue-500 p-3 text-white font-bold'>Join</button>
       </form>
