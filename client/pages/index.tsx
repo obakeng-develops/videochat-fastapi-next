fa7f52f74bdb98bacd4c2aca014e44b5f46f6883
Obakeng <mosadiobakeng7@gmail.com>
2022-08-11 12:07:08 +0200

    feat: add state for room token

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,6 +4,8 @@ import axios from 'axios'
 
 const Home: NextPage = () => {
 
+  const [roomToken, setRoomToken] = useState(null);
+
   const handleSubmit = async (event: any) => {
     event.preventDefault();
 
